af74812bbe564eb50bbc3523674ca2dbc45da1e1
* <redjhomel32@gmail.com>
2024-05-25 21:08:50 +0800

    updated

---
+++ index.js
@@ -0,0 +1,15 @@
+const section = document.querySelectorAll("section");
+
+window.onscroll = () => {
+  section.forEach(sec => {
+    let top = window.scrollY;
+    let offSet = sec.offsetTop - 160;
+    let height = sec.offsetHeight;
+    
+    if (top > offSet && top < offSet + height) {
+      sec.classList.add("start-animation");
+    } else {
+      sec.classList.remove("start-animation");
+    }
+  });
+};
